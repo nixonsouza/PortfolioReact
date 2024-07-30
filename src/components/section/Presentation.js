991258dd4ea0fa52a6342175a0a182aecacfb4ac
nixonsouza <nixon.souza@icloud.com>
2024-07-30 11:29:03 -0300

    versao-2

--- src/components/section/Presentation.js
+++ src/components/section/Presentation.js
@@ -1,11 +1,42 @@
 import ButtonA from '../elements/ButtonA'
 import styles from './Presentation.module.css'
+import { useState, useEffect } from 'react'
 
 function Presentation(){
+
+    const [text, setText] = useState('');
+    const toRotate = ['eu sou Nixon Souza!', 'eu sou Desenvolvedor Full Stack', 'eu sou Analista de Sistemas', 'eu sou Engenheiro de Software'];
+    const [loop, setLoop] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const [delta, setDelta] = useState(100);
+
+    useEffect(() =>{
+        let timer = setInterval(()=>{
+            toType()
+        }, delta)
+        return() => {clearInterval(timer)}
+    }, [text])
+
+    const toType = () =>{
+        let i = loop % toRotate.length;
+        let fullText = toRotate[i];
+        let updateText = isDeleting? fullText.substring(0, text.length -1) : fullText.substring(0, text.length +1);
+        
+        setText(updateText);
+
+        if(!isDeleting && updateText === fullText){
+            setIsDeleting(true);
+            setDelta(50);
+        }else if (isDeleting && updateText === ''){
+            setIsDeleting(false);
+            setDelta(100);
+            setLoop(loop +1);
+        }
+    }
     return (
     <div className={styles.presentation} id="Presentation">
         <h4><strong>Bem-vindo ao meu Portfólio</strong></h4>
-        <h1>Olá, eu sou Nixon!</h1>
+        <h1>Olá, {text}</h1>
         <p>
             Sou apaixonado por tecnologia, sempre atento as inovações tecnológicas <br/>
             para aprimorar minhas habilidades e agregar novos conhecimentos. <br/>
